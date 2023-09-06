1582e52e5eb9493df1ffee67cf19a4c44a3112c8
Alexander Kilyakov <sosed_a00@mail.ru>
2023-09-07 00:59:34 +0300

    Готова рабочая версия

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 const selectors = {
+  btnClear: "[data-click='clear']",
   rangeValue: "#rangeValue",
-  dataClick: "[data-click]",
+  dataMode: "[data-mode]",
+  dataModeActive: ".active[data-mode]",
   rangeInput: "#range",
+  mode: ".btn.active",
   canvas: ".canvas",
   color: "#color",
+  filledElement: ".canvas [style*=background-color]",
 };
 
 const active = "active";
 
 const rangeValue = document.querySelector(selectors.rangeValue);
 const rangeInput = document.querySelector(selectors.rangeInput);
-const dataClick = document.querySelectorAll(selectors.dataClick);
+const dataMode = document.querySelectorAll(selectors.dataMode);
 const canvas = document.querySelector(selectors.canvas);
 const color = document.querySelector(selectors.color);
+const btnClear = document.querySelector(selectors.btnClear);
 
 function followTheRange() {
   rangeValue.textContent = `${rangeInput.value} x ${rangeInput.value}`;
@@ -37,13 +42,77 @@ function createPixel(width) {
   return pixel;
 }
 
+function drawing({ target }) {
+  target.style.backgroundColor = getColor();
+}
+
+function startDrawing() {
+  canvas.addEventListener("mousemove", drawing);
+}
+
+function endDrawing() {
+  canvas.removeEventListener("mousemove", drawing);
+}
+
+function getColor() {
+  const mode = document.querySelector(selectors.mode);
+
+  switch (mode.dataset.mode) {
+    case "standard":
+      return color.value;
+    case "rainbow":
+      return getRandomColor();
+    case "eraser":
+      return "var(--canvas)";
+  }
+
+  return "#ffffff";
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+function setButtonEvent(btn) {
+  const btnActive = document.querySelector(selectors.dataModeActive);
+
+  if (btnActive) {
+    btnActive.classList.remove(active);
+  }
+
+  btn.classList.add(active);
+}
+
+function clearCanvas() {
+  const filledElements = document.querySelectorAll(selectors.filledElement);
+
+  if (!filledElements.length) return false;
+
+  filledElements.forEach((element) => {
+    element.style.backgroundColor = "";
+  });
+}
+
 function start() {
   followTheRange();
   createPixels();
 
   rangeInput.addEventListener("change", createPixels);
   rangeInput.addEventListener("input", followTheRange);
-  //   canvas.addEventListener("input", followTheRange);
+  btnClear.addEventListener("click", clearCanvas);
+  canvas.addEventListener("mousedown", startDrawing);
+  canvas.addEventListener("mouseup", endDrawing);
+
+  dataMode.forEach((btn) => {
+    btn.addEventListener("click", () => setButtonEvent(btn));
+  });
 }
 
 start();
